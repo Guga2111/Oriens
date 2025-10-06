55b2635f69a84f9a9bacb13ea6758df49b99df6d
luisgustavo <luisgosampaio@gmail.com>
2025-10-06 18:43:41 -0300

    feat: creating the CORS config class

--- oriens-api/src/main/java/com/oriens/oriens_api/CorsConfig.java
+++ oriens-api/src/main/java/com/oriens/oriens_api/CorsConfig.java
@@ -1,4 +1,19 @@
 package com.oriens.oriens_api;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") //
+                .allowedOrigins("http://localhost:5173", "https://oriens.luisgosampaio.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
 }
