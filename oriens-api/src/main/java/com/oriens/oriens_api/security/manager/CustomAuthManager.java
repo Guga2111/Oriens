c9a1b5cb7bec07575c152f52c9e7596848c52ca6
luisgustavo <luisgosampaio@gmail.com>
2025-10-07 10:24:44 -0300

    fix: error login on checking password credentials

--- oriens-api/src/main/java/com/oriens/oriens_api/security/manager/CustomAuthManager.java
+++ oriens-api/src/main/java/com/oriens/oriens_api/security/manager/CustomAuthManager.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -26,7 +25,7 @@ public class CustomAuthManager implements AuthenticationManager {
 
         User user = userService.getUser(authentication.getName());
 
-        if (bCryptPasswordEncoder.matches(authentication.getCredentials().toString(), user.getPassword())) {
+        if (!bCryptPasswordEncoder.matches(authentication.getCredentials().toString(), user.getPassword())) {
             throw new BadCredentialsException("The password you provided is incorrect!");
         }
 
