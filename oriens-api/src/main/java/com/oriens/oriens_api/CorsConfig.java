2684b74310b164c04c1739347d4f8fffb08fffe9
luisgustavo <luisgosampaio@gmail.com>
2025-10-11 18:30:35 -0300

    feat: Enabling http:// network protocol for a while

--- oriens-api/src/main/java/com/oriens/oriens_api/CorsConfig.java
+++ oriens-api/src/main/java/com/oriens/oriens_api/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //
-                .allowedOrigins("http://localhost:5173", "https://oriens.luisgosampaio.com")
+                .allowedOrigins("http://localhost:5173", "https://oriens.luisgosampaio.com", "http://oriens.luisgosampaio.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
