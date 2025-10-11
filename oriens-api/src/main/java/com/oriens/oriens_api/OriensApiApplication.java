6d7cde6cae18521d588a3d43e3d9aa5ce78be572
luisgustavo <luisgosampaio@gmail.com>
2025-10-11 18:26:02 -0300

    feat: Enabling Spring Scheduler and setting the default timezone for Recife (my place :D)

--- oriens-api/src/main/java/com/oriens/oriens_api/OriensApiApplication.java
+++ oriens-api/src/main/java/com/oriens/oriens_api/OriensApiApplication.java
@@ -1,12 +1,23 @@
 package com.oriens.oriens_api;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.TimeZone;
+
+@EnableScheduling
 @SpringBootApplication
 public class OriensApiApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Recife"));
+		System.out.println("Correction on zones: " + TimeZone.getDefault().getID());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OriensApiApplication.class, args);
