2863b73907919998ef98d57d67723dc18a70c23c
luisgustavo <luisgosampaio@gmail.com>
2025-10-14 20:57:47 -0300

    feat: adding PATCH http response to CORS

--- oriens-api/src/main/java/com/oriens/oriens_api/CorsConfig.java
+++ oriens-api/src/main/java/com/oriens/oriens_api/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //
                 .allowedOrigins("http://localhost:5173", "https://oriens.luisgosampaio.com", "http://oriens.luisgosampaio.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true);
